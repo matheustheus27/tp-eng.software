f661deeb0e46bb9031a45bebe534922ae1efcc6b
Matheus Thiago <matheustheus27@gmail.com>
2023-06-30 22:46:40 -0300

    UserType and OrderStatus

--- pizzariaWeb/appPizzaria/models.py
+++ pizzariaWeb/appPizzaria/models.py
@@ -3,14 +3,14 @@ from enum import Enum
 
 ## User
 class UserType(Enum):
-    COMMON = 1
-    MANAGER = 2
+    COMMON = (1, "Cliente")
+    MANAGER = (2, "Funcionario")
 
 class User(models.Model):
     cpf = models.CharField(max_length=11)
     name = models.CharField(max_length=50)
     date_birth = models.DateField()
-    type = models.IntegerField(default=1)
+    type = models.IntegerField(choices=[(user_type.value[0], user_type.value[1]) for user_type in UserType], default=UserType.COMMON.value[0])
     email = models.EmailField()
     phone = models.CharField(max_length=14)
     address = models.CharField(max_length=125)
@@ -23,6 +23,7 @@ class Pizza(models.Model):
     flavor = models.CharField(max_length=50)
     size = models.CharField(max_length=3)
     type = models.CharField(max_length=50)
+    price = models.FloatField(default=0)
 
     def __str__(self):
         return self.flavor
@@ -31,6 +32,7 @@ class Drink(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     size = models.CharField(max_length=8)
+    price = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
@@ -38,6 +40,7 @@ class Drink(models.Model):
 class Promotion(models.Model):
     pizza = models.ForeignKey('Pizza', on_delete=models.CASCADE, default=None)
     drink = models.ForeignKey('Drink', on_delete=models.CASCADE, default=None)
+    price = models.FloatField(default=0)
 
     def __str__(self):
         return self.id
@@ -52,20 +55,19 @@ class Menu(models.Model):
 
 ## Order
 class OrderStatus(Enum):
-    PENDING: 1
-    APPROVED: 2
-    REPROVED: 3
-    IN_PREPARATION: 4
-    AT_DELIVERY_ROUTE: 5
-    DELIVERED: 6
-    RETURNED: 7
+    PENDING = (1, "Pendente")
+    APPROVED = (2, "Aceito")
+    REPROVED = (3, "Recusado")
+    IN_PREPARATION = (4, "Em Preparação")
+    AT_DELIVERY_ROUTE = (5, "Em Rota")
+    DELIVERED = (6, "Entregue")
+    RETURNED = (7, "Devolvido")
 
 class Item(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, null=True, default=None)
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE, null=True, default=None)
     promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE, null=True, default=None)
     amount = models.IntegerField()
-    price = models.FloatField()
 
 class ShoppingCart(models.Model):
     items = models.ManyToManyField(Item)
@@ -78,7 +80,7 @@ class Order(models.Model):
     number = models.IntegerField()
     order = models.ForeignKey('ShoppingCart', on_delete=models.CASCADE, default=None)
     user = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
-    status = models.IntegerField(default=1)
+    status = models.IntegerField(choices=[(status.value[0], status.value[1]) for status in OrderStatus], default=OrderStatus.PENDING.value[0])
 
     def __str__(self):
         return self.number
